a4621b6ccb4d82488e3b288b3632d82ba827c8ed
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2022-07-08 15:48:02 -0400

    some css improve and Link fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link as RouteLink, useNavigate} from 'react-router-dom'
+import {Link as ReachLink, useNavigate} from 'react-router-dom'
 import {
   Box,
   Button,
@@ -32,6 +32,8 @@ function Login() {
 
     setLogin(true)
 
+
+    // Replace the logic with the Backend Validation.
     setTimeout( () => {
       setLogin(false)
 
@@ -55,7 +57,7 @@ function Login() {
   return (
     <Box w='100vw' h='100vh' display='flex' style={{backgroundImage: 'linear-gradient(160deg, #0c669a 0%, #26e6d1 100%)'}}>
 
-      <Box maxWidth='2xl' maxH='4xl' m='auto' borderWidth={1} borderRadius={10} shadow='md' px={'10'} py={'16'} bg='white' >
+      <Box maxWidth='2xl' maxH='4xl' m='auto' borderWidth={1} borderRadius={10} shadow='dark-lg' px={'10'} py={'16'} bg='white' >
         <Text color='gray.500' casing='uppercase'>Welcome Back</Text>
         
         <Heading fontSize='4xl'>
@@ -67,15 +69,15 @@ function Login() {
 
         <form onSubmit={register}>
         <Box mt={3} w={'100%'} >
-            <Text mb={'-3'} opacity={0.5} fontSize={'10'}  >Email</Text>
-            <Input variant={'flushed'} value={email} onChange={(e)=>{setEmail(e.target.value)}} type={'email'} isRequired/>
+            <Text mb={'-3'} opacity={0.5} fontSize={'10'}>Email</Text>
+            <Input variant={'flushed'} value={email} onChange={(e)=>{setEmail(e.target.value)}} type={'email'} isRequired _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
         </Box>
         <Box mt={3} w={'100%'} >
-          <Text mb={'-3'} opacity={0.5} fontSize={'10'}  >Password</Text>
+          <Text mb={'-3'} opacity={0.5} fontSize={'10'}>Password</Text>
           <InputGroup>
-          <Input variant={'flushed'} value={password} onChange={(e)=>{setPassword(e.target.value)}} type={showPassword ? 'text' : 'password'} isRequired/>
+          <Input variant={'flushed'} value={password} onChange={(e)=>{setPassword(e.target.value)}} type={showPassword ? 'text' : 'password'} isRequired  _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
           <InputRightElement>
-          <IconButton variant={'unstyled'} color={'cyan.400'} icon={showPassword ? <FaRegEyeSlash /> : <FaRegEye />} onClick={()=>{setShowPassword(!showPassword)}} />
+          <IconButton variant={'unstyled'} color={'cyan.800'} icon={showPassword ? <FaRegEyeSlash /> : <FaRegEye />} onClick={()=>{setShowPassword(!showPassword)}} />
           </InputRightElement>
           </InputGroup>
         </Box>
@@ -83,7 +85,7 @@ function Login() {
         <Button w={'47%'} colorScheme='gray' mr={'6%'}  shadow={'base'} >Go back</Button>
         <Button w={'47%'} colorScheme='cyan' shadow={'base'} color={'gray.900'} isLoading={login ? true : false} type='submit'>Continue</Button>
         </Box>
-        <Text textAlign={'center'} mt={'5'}>Are you new? <Link color='cyan.400'> <RouteLink to='/register'>Create Account</RouteLink></Link></Text>
+        <Text textAlign={'center'} mt={'5'}>Are you new? <Link color='cyan.400' as={ReachLink} to={'/register'}>Create Account</Link></Text>
         </form>
       </Box>
       
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link as RouteLink, useNavigate} from 'react-router-dom'
+import {Link as ReachLink, useNavigate} from 'react-router-dom'
 import {
   Box,
   Button,
@@ -34,6 +34,8 @@ function Register() {
 
     setCreatingAccount(true)
 
+    //Replace logic with a backend validation.
+
     setTimeout( () => {
       setCreatingAccount(false)
 
@@ -57,7 +59,7 @@ function Register() {
   return (
     <Box w='100vw' h='100vh' display='flex' style={{backgroundImage: 'linear-gradient(160deg, #0c669a 0%, #26e6d1 100%)'}}>
 
-      <Box maxWidth='2xl' maxH='4xl' m='auto' borderWidth={1} borderRadius={10} shadow='md' px={'10'} py={'16'} bg='white' >
+      <Box maxWidth='2xl' maxH='4xl' m='auto' borderWidth={1} borderRadius={10} shadow={'dark-lg'} px={'10'} py={'16'} bg='white' >
         <Text color='gray.500' casing='uppercase'>Start for free</Text>
         
         <Heading fontSize='4xl'>
@@ -71,23 +73,23 @@ function Register() {
         <Box display='flex' w={'100%'} mt={'6'}>
           <Box mt={3} mr={'6%'} w={'47%'} >
             <Text mb={'-3'} opacity={0.5} fontSize={'10'} >Name</Text>
-            <Input variant={'flushed'} value={name} onChange={(e)=>{setName(e.target.value)}} type={'text'} isRequired />
+            <Input variant={'flushed'} value={name} onChange={(e)=>{setName(e.target.value)}} type={'text'} isRequired _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
           </Box>
           <Box mt={3} w={'47%'} >
             <Text mb={'-3'} opacity={0.5} fontSize={'10'}  >Lastname</Text>
-            <Input variant={'flushed'} value={lastname} onChange={(e)=>{setLastname(e.target.value)}} type={'text'} isRequired/>
+            <Input variant={'flushed'} value={lastname} onChange={(e)=>{setLastname(e.target.value)}} type={'text'} isRequired _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
           </Box>
         </Box>
         <Box mt={3} w={'100%'} >
             <Text mb={'-3'} opacity={0.5} fontSize={'10'}  >Email</Text>
-            <Input variant={'flushed'} value={email} onChange={(e)=>{setEmail(e.target.value)}} type={'email'} isRequired/>
+            <Input variant={'flushed'} value={email} onChange={(e)=>{setEmail(e.target.value)}} type={'email'} isRequired _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
         </Box>
         <Box mt={3} w={'100%'} >
           <Text mb={'-3'} opacity={0.5} fontSize={'10'}  >Password</Text>
           <InputGroup>
-          <Input variant={'flushed'} value={password} onChange={(e)=>{setPassword(e.target.value)}} type={showPassword ? 'text' : 'password'} isRequired/>
+          <Input variant={'flushed'} value={password} onChange={(e)=>{setPassword(e.target.value)}} type={showPassword ? 'text' : 'password'} isRequired _focus={{borderColor: 'cyan.400'}} color='cyan.800'/>
           <InputRightElement>
-          <IconButton variant={'unstyled'} color={'cyan.400'} icon={showPassword ? <FaRegEyeSlash /> : <FaRegEye />} onClick={()=>{setShowPassword(!showPassword)}} />
+          <IconButton variant={'unstyled'} color={'cyan.800'} icon={showPassword ? <FaRegEyeSlash /> : <FaRegEye />} onClick={()=>{setShowPassword(!showPassword)}} />
           </InputRightElement>
           </InputGroup>
         </Box>
@@ -95,7 +97,7 @@ function Register() {
         <Button w={'47%'} colorScheme='gray' mr={'6%'}  shadow={'base'} >Cancel</Button>
         <Button w={'47%'} colorScheme='cyan' shadow={'base'} color={'gray.900'} isLoading={creatingAccount ? true : false} type='submit'>Create Account</Button>
         </Box>
-        <Text textAlign={'center'} mt={'5'}>Already a member? <Link color='cyan.400'> <RouteLink to='/login'>Log In</RouteLink></Link></Text>
+        <Text textAlign={'center'} mt={'5'}>Already a member? <Link color='cyan.400' as={ReachLink} to={'/login'}>Log In</Link></Text>
         </form>
       </Box>
       
