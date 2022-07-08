2e19997f78e33c27e194764df829ac096691a229
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2022-07-08 00:24:32 -0400

    routes for pages created, home, dashboard, login, register, *

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-       <h1>Hello! Thank you for this :) </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
